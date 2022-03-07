fa84f33af6a65f64b984990b56d3c0628fc175be
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-03-07 15:25:45 +0100

    Issue

--- Runtime/InputActions/AnyKeyInputAction.cs
+++ Runtime/InputActions/AnyKeyInputAction.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using UnityEngine.InputSystem;
-using static UnityEngine.InputSystem.InputAction;
+﻿using UnityEngine.InputSystem;
 
 namespace Juce.Input.InputActions
 {
     public class AnyKeyInputAction
     {
-        private readonly InputAction inputAction;
-
-        public event Action<CallbackContext> Performed;
+        public InputAction InputAction { get; } 
 
         public AnyKeyInputAction()
         {
-            inputAction = new InputAction(binding: "/*/<button>", type: InputActionType.Button);
-            inputAction.AddBinding("/<Gamepad>/*/*");
-
-            inputAction.performed += Performed;
+            InputAction = new InputAction(binding: "/*/<button>", type: InputActionType.Button);
+            InputAction.AddBinding("/<Gamepad>/*/*");
         }
     }
 }
