c3a77424d873804a9d9ff565563d3d08d9c179c6
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-03-11 16:28:58 +0100

    Working on parsing xml

---
+++ Runtime/Tags/InputTagsParser.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace Juce.Input.Tags
+{
+    public static class InputTagsParser
+    {
+        public static List<XmlNode> Parse(string text)
+        {
+            List<XmlNode> nodes = new List<XmlNode>();
+
+            List<string> xmlPortions = FindXmlPortions(text);
+
+            foreach (string portion in xmlPortions)
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+
+                try
+                {
+                    xmlDocument.Load(new StringReader(portion));
+                }
+                catch
+                {
+                    continue;
+                }
+
+                XmlNodeList nodeList = xmlDocument.SelectNodes("input");
+
+                foreach(XmlNode node in nodeList)
+                {
+                    nodes.Add(node);
+                }
+            }
+
+            return nodes;
+        }
+
+        public static string ReplaceTag(string text, string tagContent, string replacement)
+        {
+            string fullTag = string.Format("<input>{0}</input>", tagContent);
+
+            return text.Replace(fullTag, replacement);
+        }
+
+        public static List<string> FindXmlPortions(string text)
+        {
+            List<string> portions = new List<string>();
+
+            MatchCollection matchCollection = Regex.Matches(text, @"<([^>]+)>[^<]*</(\1)>");
+
+            foreach (Match match in matchCollection)
+            {
+                portions.Add(match.Value);
+            }
+
+            return portions;
+        }
+    }
+}
---
+++ Runtime/TextMeshPro/TextMeshProInputParser.cs
@@ -0,0 +1,70 @@
+﻿#if JUCE_TEXT_MESH_PRO_EXTENSIONS
+
+using Juce.CoreUnity.ControlSchemeIcons;
+using Juce.Input.Tags;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Juce.Input.TextMeshPro
+{
+    public class TextMeshProInputParser : MonoBehaviour
+    {
+        [SerializeField] private TMPro.TextMeshProUGUI text = default;
+        [SerializeField] private InputActionAsset inputActionAsset = default;
+        [SerializeField] private DeviceControlSchemeIcons deviceControlSchemeIcons = default;
+
+        private void Awake()
+        {
+            UpdateText();
+        }
+
+        private void UpdateText()
+        {
+            List<XmlNode> nodes = InputTagsParser.Parse(text.text);
+
+            foreach (XmlNode node in nodes)
+            {
+                foreach (InputActionMap inputActionMap in inputActionAsset.actionMaps)
+                {
+                    foreach (InputAction inputAction in inputActionMap.actions)
+                    {
+                        foreach (InputBinding inputBinding in inputAction.bindings)
+                        {
+                            bool isInputAction = string.Equals(inputBinding.action, node.InnerText);
+
+                            if (!isInputAction)
+                            {
+                                continue;
+                            }
+
+                            bool found = deviceControlSchemeIcons.TryGet(
+                                inputBinding.effectivePath,
+                                out IControlSchemeIconItem controlSchemeIconItem
+                                );
+
+                            if(!found)
+                            {
+                                continue;
+                            }
+
+                            string iconPath = TextMeshProUtils.BuildMarkdownForSprite(
+                                controlSchemeIconItem.AtlasAsset,
+                                controlSchemeIconItem.AtlasName
+                                );
+
+                            text.text = InputTagsParser.ReplaceTag(
+                                text.text,
+                                node.InnerText,
+                                iconPath
+                                );
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#endif 
