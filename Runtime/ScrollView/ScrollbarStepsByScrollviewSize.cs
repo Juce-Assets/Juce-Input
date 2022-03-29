bfae502567d26ef64013d30d3f5fba307d3a3ee9
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2022-03-29 16:40:17 +0200

    Steps

---
+++ Runtime/ScrollView/ScrollbarStepsByScrollviewSize.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Juce.Input.ScrollView
+{
+    public class ScrollbarStepsByScrollviewSize : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private Scrollbar scrollbar = default;
+        [SerializeField] private RectTransform scrollRectContent = default;
+
+        [Header("Values")]
+        [SerializeField, Min(0)] private float stepDistance = default;
+
+        private void Update()
+        {
+            TrySetStepDistance();
+        }
+
+        private void TrySetStepDistance()
+        {
+            float stepsCount = 0;
+
+            if(stepDistance > 0)
+            {
+                stepsCount = scrollRectContent.sizeDelta.y / stepDistance;
+            }
+
+            scrollbar.numberOfSteps = (int)stepsCount;
+        }
+    }
+}
